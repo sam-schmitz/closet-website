5cbb48b2ebad6d829bf4753d4c5d12078ac174bd
sam-schmitz <116108955+sam-schmitz@users.noreply.github.com>
2024-05-25 21:01:31 -0500

    Added in a room and a product

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import dresser from './images/dresser.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+          <div className="Room">
+              <Product
+                  type={"dresser"}
+              />
+          </div></>
   );
+}
+
+function Product({ type }) {
+    return (
+        <>
+            <a href="https://www.google.com" className="Product">
+                <img src={dresser} alt={type} />
+            </a>
+        </>
+    );
 }
 
 export default App;
